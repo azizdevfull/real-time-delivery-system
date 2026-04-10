1506b11eaa2aa8db97990d06f32a2c1ee09a4cf5
Azizbek Isroilov <aziz16110904@gmail.com>
2026-04-10 16:56:12 +0500

    pint

--- driver-service/app/Models/Driver.php
+++ driver-service/app/Models/Driver.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 #[Fillable(['name', 'status'])]
 class Driver extends Model
 {
     use HasFactory;
+
     const STATUS_AVAILABLE = 'available';
+
     const STATUS_BUSY = 'busy';
 }
